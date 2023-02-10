e6bc4c76abda64f1221aca68578295fed2e38ba3
Ronald Loyko <linkbox@ronald.loyko.me>
2023-02-11 00:12:43 +0200

    chore: update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import Application from "./Application";
 import "./i18n";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <Application />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
