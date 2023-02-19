0a5d71644d0240aa06ece5406ac21edf7ebe4801
Ronald Loyko <linkbox@ronald.loyko.me>
2023-02-19 03:21:39 +0200

    feat: add search by tags

--- src/components/Overlays/Modals/Search.tsx
+++ src/components/Overlays/Modals/Search.tsx
@@ -3,16 +3,23 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonItem,
   IonModal,
   IonSearchbar,
+  IonSelect,
+  IonSelectOption,
   IonTitle,
   IonToolbar,
+  SelectChangeEventDetail,
   type SearchbarChangeEventDetail,
 } from "@ionic/react";
+import { pricetag } from "ionicons/icons";
 import { useMemo, useRef, useState, type FC } from "react";
 import { useTranslation } from "react-i18next";
-import { EMPTY_TEXT } from "../../../data/constants";
+import { EMPTY_TAGS, EMPTY_TEXT } from "../../../data/constants";
 import { useAction, useDispatch, useSelector } from "../../../store";
+import { Id } from "../../../store/items";
 import EmptyState from "./Search/EmptyState";
 import Links from "./Search/Links";
 
@@ -20,22 +27,31 @@ export default (function Search() {
   const dispatch = useDispatch();
   const open = useSelector((state) => state.ui.searchModalOpen);
   const links = useSelector((state) => state.items.links);
+  const availableTags = useSelector((state) => state.items.tags);
   const statusBarHeight = useSelector((state) => state.ui.statusBarHeight);
+  const useTags = useSelector((state) => state.ui.useTags);
   const searchBarElement = useRef<HTMLIonSearchbarElement>(null);
   const { t } = useTranslation();
   const { toggleSearchModal } = useAction();
   const [term, setTerm] = useState(EMPTY_TEXT);
+  const [tags, setTags] = useState(EMPTY_TAGS as Id[]);
 
+  const processTags = useTags && availableTags.length;
   const processedTerm = term.trim().toLowerCase();
-  const filteredLinks = useMemo(
-    () =>
-      links.filter(
-        ({ name, description }) =>
-          name.trim().toLowerCase().includes(processedTerm) ||
-          description.trim().toLowerCase().includes(processedTerm)
-      ),
-    [links, processedTerm]
-  );
+  const filteredLinks = useMemo(() => {
+    if (!processTags && !processedTerm) {
+      return [];
+    }
+
+    return links.filter((link) =>
+      [
+        processedTerm && link.name.trim().toLowerCase().includes(processedTerm),
+        processedTerm &&
+          link.description.trim().toLowerCase().includes(processedTerm),
+        processTags ? tags.some((tag) => link.tags.includes(tag)) : false,
+      ].some((is) => is === true)
+    );
+  }, [links, processedTerm, tags]);
 
   function onTermChange({ detail }: CustomEvent<SearchbarChangeEventDetail>) {
     setTerm(detail.value ?? EMPTY_TEXT);
@@ -47,10 +63,17 @@ export default (function Search() {
 
   function onBeforeShow() {
     setTerm(EMPTY_TEXT);
+    setTags(EMPTY_TAGS);
   }
 
   function onShow() {
     searchBarElement.current?.setFocus();
+  }
+
+  function onTagsChange({
+    detail,
+  }: CustomEvent<SelectChangeEventDetail<Id[]>>) {
+    setTags(detail.value as Id[]);
   }
 
   return (
@@ -81,9 +104,41 @@ export default (function Search() {
             }}
           />
         </IonToolbar>
+        {processTags && (
+          <IonToolbar>
+            <IonItem lines="none">
+              <IonIcon
+                slot="start"
+                icon={pricetag}
+                style={{
+                  fontSize: "16pt",
+                  marginLeft: "4px",
+                  marginRight: "20px",
+                }}
+              />
+              <IonSelect
+                onIonChange={onTagsChange}
+                placeholder={t("overlays.modals.search.tags.placeholder")}
+                multiple={true}
+                value={tags}
+                cancelText={t("overlays.modals.search.tags.cancel")}
+                okText={t("overlays.modals.search.tags.choose")}
+                interfaceOptions={{
+                  header: t("overlays.modals.search.tags.header"),
+                }}
+              >
+                {availableTags.map(({ id, name }) => (
+                  <IonSelectOption key={id} value={id}>
+                    {name}
+                  </IonSelectOption>
+                ))}
+              </IonSelect>
+            </IonItem>
+          </IonToolbar>
+        )}
       </IonHeader>
       <IonContent>
-        {processedTerm.length && filteredLinks.length ? (
+        {filteredLinks.length ? (
           <Links links={filteredLinks} />
         ) : (
           <EmptyState />
